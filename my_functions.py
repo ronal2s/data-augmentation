3e83089618c14a1709f40f2dfaac956f4bf1f5e1
Renys Ronaldo <ronal2w@gmail.com>
2021-07-24 14:48:57 -0400

    data augumentation using tf.image

---
+++ my_functions.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+def show_image(original_image, augmented_image, title):
+    fig = plt.figure()
+    fig.suptitle(title)
+
+    original_plt = fig.add_subplot(1, 2, 1)
+
+    original_plt.set_title('original image')
+    original_plt.imshow(original_image)
+
+    augmented_plt = fig.add_subplot(1, 2, 2)
+    augmented_plt.set_title('augmented image')
+    augmented_plt.imshow(augmented_image)
+    plt.show(block=True)
